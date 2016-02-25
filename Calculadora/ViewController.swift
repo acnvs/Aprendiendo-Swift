2a971498a79a6624d18190a9895ed2655bd846ef
Ángel Cánovas <acanovasceldran@gmail.com>
2016-02-25 15:41:48 +0100

    Modificación de prueba para GitHub

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Texto de prueba para GitHub
 
+    @IBOutlet weak var estatura: UITextField!
+    @IBOutlet weak var peso: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func calcularIMC(sender: AnyObject) {
+        var IMC:Double
+        let pesoLocal:Double?
+        pesoLocal=Double(self.peso.text!)!
+        let estaturaLocal:Double=Double(self.estatura.text!)!
+        IMC=pesoLocal!/(estaturaLocal*estaturaLocal)
+        print("Resltado:\(IMC)")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
